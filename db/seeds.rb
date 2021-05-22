8d6acbec465894951491f6ef2b79b403ae3c3293
Augusto Souza <augustodesouzacardoso@gmail.com>
2021-05-22 16:52:10 -0300

    pundit installer

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,82 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'creating users...'
+users = []
+
+user1 = User.new(
+  email: Faker::Internet.email,
+  password: "abc123",
+  reporter: "true",
+  # first_name: "Felippe",
+  # last_name: "Santana"
+)
+user1.save!
+
+user2 = User.new(
+  email: Faker::Internet.email,
+  password: "abc123",
+  reporter: "false",
+  # first_name: "Augusto",
+  # last_name: "Souza"
+)
+user2.save!
+
+user3 = User.new(
+  email: Faker::Internet.email,
+  password: "abc123",
+  reporter: "false",
+  # first_name: "Pedro",
+  # last_name: "Breischiodshisdhg"
+)
+user3.save!
+
+user4 = User.new(
+  email: Faker::Internet.email,
+  password: "abc123",
+  reporter: "false",
+  # first_name: "Yuri",
+  # last_name: "Gravatá"
+)
+user4.save!
+
+users << user1
+users << user2
+users << user3
+users << user4
+
+puts 'creating articles...'
+articles = []
+
+article = Article.new(
+  title: "1 beadrom in Ile-de-France",
+  subtitle: "37-1 Passage du Ponceau, Paris, France",
+  content: "Lovely article located in the heart of paris, walking distance form the Seine river",
+  communities: ["Rocinha", "Vidigal"],
+  interests: ["Sports", "Culture"]
+)
+article.user = users.sample
+article.save!
+articles << article
+
+article = Article.new(
+  title: "Apartment in front of the beach",
+  subtitle: "Av. Vieira Souto, 366, Ipanema, Rio de Janeiro, Brazil",
+  content: "Big apartment with panoramic view for the best urban beach in ther world. Come to have an unique experince",
+  communities: ["Borel", "Macacos"],
+  interests: ["Sports", "Finance"]
+)
+article.user = users.sample
+article.save!
+articles << article
+
+article = Article.new(
+  title: "Penthouse in Copacabana",
+  subtitle: "Rodolfo Dantas 85, Rio de Janeiro, Brazil",
+  content: "Modern and spacious 2 bedroom penthouse ideally located in Copacabana. This penthouse provides the perfect private retreat in which to relax yourself: stylish interior design, bright rooms and a large terrace with a swimming-pool.",
+  communities: ["Antares", "Barbante", "Rola"],
+  interests: ["Security"]
+)
+article.user = users.sample
+article.save!
+articles << article
