142399eef7fef8b2848f84bb7a8365c26f4ee6d9
Pedro Resende Breitschaft <pedro_brei@hotmail.com>
2021-05-27 20:10:22 -0300

    Pages controller with news method, profile -> edit and update

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,24 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def news
+    @articles = policy_scope(Article).order(created_at: :desc)
+  end
+  
+  def profile
+    edit
+  end
+
+  def edit
+    @user = current_user
+    authorize @user 
+  end
+
+  def update
+    @user.update(user_params)
+    authorize @user 
+    redirect_to profile_path
+  end
+
 end
