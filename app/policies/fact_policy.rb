991678ead1f6d0a108b6a07553c974074423ed77
Augusto Souza <augustodesouzacardoso@gmail.com>
2021-06-03 19:37:52 -0300

    added pundit to facts

---
+++ app/policies/fact_policy.rb
@@ -0,0 +1,27 @@
+class FactPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+  
+  def create?
+    user.reporter
+  end
+
+  def show?
+    !user.reporter
+  end
+
+  def update?
+    user.reporter
+  end
+
+  def destroy?
+    user.reporter
+  end
+
+  def news?
+    !user.reporter
+  end
+end
