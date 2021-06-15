c4b50c1e5a8e47e3d75413b2b04c8f0875d2598d
felippe menezes <felippemenezes1982@gmail.com>
2021-06-15 18:38:23 -0300

    Adjusted until article 10 and all reporters

--- db/seeds.rb
+++ db/seeds.rb
@@ -32,8 +32,8 @@ user1 = User.new(
   email: "teste1@favelazap.com",
   password: "abc123",
   reporter: "true",
-  communities: ["Rocinha", "Vidigal"],
-  interests: ["Sports", "Culture"],
+  communities: ["Rocinha", "Vidigal", "Terreirão"],
+  interests: ["Social", "Sports", "Health", "Politics"],
   first_name: "Manoel",
   last_name: "Soares"
 )
@@ -44,8 +44,8 @@ user2 = User.new(
   email: "teste2@favelazap.com",
   password: "abc123",
   reporter: "true",
-  communities: ["Borel", "Formiga"],
-  interests: ["Sports", "Economy"],
+  communities: ["Borel", "Formiga", "Macacos"],
+  interests: ["Culture", "Security", "Others"],
   first_name: "Nina",
   last_name: "Simone"
 )
@@ -56,8 +56,8 @@ user3 = User.new(
   email: "teste3@favelazap.com",
   password: "abc123",
   reporter: "true",
-  communities: ["Juramento", "Macacos"],
-  interests: ["Sports", "Economy"],
+  communities: ["Alemão", "Antares", "Jacaré", "Chapadão"],
+  interests: ["Health", "Economy", "Security"],
   first_name: "Sevana",
   last_name: "Bilquily"
 )
@@ -83,7 +83,7 @@ user5 = User.new(
   communities: ["Rocinha", "Vidigal"],
   interests: ["Health", "Security"],
   first_name: "Pedro",
-  last_name: "Breischspacsa"
+  last_name: "Breitschaft"
 )
 user5.avatar.attach(io: photo5, filename: 'gil.jpg', content_type: 'image/jpg')
 user5.save!
@@ -142,7 +142,7 @@ article_photo3 = URI.open('https://img-vozdascomunidade.s3.sa-east-1.amazonaws.c
 article_photo4 = URI.open('http://43.mostra.org/_img/_filmes/186.jpg')
 article_photo5 = URI.open('https://www.vozdascomunidades.com.br/wp-content/uploads/2020/06/rafael-oliveira.jpeg')
 article_photo6 = URI.open('https://img-vozdascomunidade.s3.sa-east-1.amazonaws.com/wp-content/uploads/2021/03/10160213/Vacina-Covid19-Coranavirus-.jpg234-scaled-1-1.jpg')
-article_photo7 = URI.open('https://images.unsplash.com/photo-1565807511924-ba5ec83f2e09?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib')
+article_photo7 = URI.open('https://media.gettyimages.com/videos/volunteers-deliver-packs-of-food-to-residents-in-paraisopolis-favela-video-id1214783665?s=640x640')
 article_photo8 = URI.open('https://veja.abril.com.br/wp-content/uploads/2021/06/DOM-08.jpg.jpg?quality=70&strip=info&resize=680,453')
 article_photo9 = URI.open('https://img-vozdascomunidade.s3.sa-east-1.amazonaws.com/wp-content/uploads/2021/06/04140528/E3DLTFoXIAEGA65.jpeg')
 article_photo10 = URI.open('https://img-vozdascomunidade.s3.sa-east-1.amazonaws.com/wp-content/uploads/2021/05/13102443/ARY-BARROSO.png')
@@ -162,21 +162,21 @@ article = Article.new(
   subtitle: "In this new cycle, UPP officers in Borel will carry out cultural activities",
   content: "After 5 years without any agreement to help the continuity of activities, the Social Attitude project finally found a partner to continue its work in the Borel community: the community's UPP. Police officers from the favela's police unit will be acting as teachers of social projects, after more than a year without socio-educational activities in the space.",
   communities: ["Borel", "Formiga"],
-  interests: ["Sports", "Economy"]
+  interests: ["Social", "culture", "Security"]
 )
-article.user = reporters.sample
+article.user = reporters[1]
 article.photo.attach(io: article_photo1, filename: 'article1.jpg', content_type: 'image/jpg')
 article.save!
 articles << article
 
 article = Article.new(
-  title: "Project unveils values of tennis at Alemão",
+  title: "Project unveils values of tennis at Alemão and Antares",
   subtitle: "Teachers transform the social perspectives of 55 young people in the community",
-  content: "With the intense noise of tennis balls being frequently thrown by rackets, the agitation of every Tuesday, Thursday and Saturday in the Olympic Village of Complexo do Alemão, in the North Zone of Rio de Janeiro, receives the presence of the restlessness of 55 young people who participate in the Favela Tennis & Culture. Between training sessions and conversations with the teachers, the children awaken to values and goals that go beyond the dispute of game sets: the perception that it is possible to break down social barriers.",
-  communities: ["Juramento", "Macacos"],
-  interests: ["Sports", "Economy"]
+  content: "With the intense noise of tennis balls being frequently thrown by rackets, the agitation of every Tuesday, Thursday and Saturday in the Olympic Village of Complexo do Alemão and Antares, in the North Zone of Rio de Janeiro, receives the presence of the restlessness of 55 young people who participate in the Favela Tennis & Culture. Between training sessions and conversations with the teachers, the children awaken to values and goals that go beyond the dispute of game sets: the perception that it is possible to break down social barriers.",
+  communities: ["Alemão", "Antares"],
+  interests: ["Sports", "Social", "Culture"]
 )
-article.user = reporters.sample
+article.user = reporters[2]
 article.photo.attach(io: article_photo2, filename: 'article2.jpg', content_type: 'image/jpeg')
 article.save!
 articles << article
@@ -185,34 +185,34 @@ article = Article.new(
   title: "Vidigal receives new garbage containers",
   subtitle: "Comlurb started distribution at strategic points in the community",
   content: "Among the indicators that point to improvements in living conditions in Rio's favelas, the collection and correct disposal of waste are certainly one of the main demands. Aiming to improve this essential recycling service, Comlurb started the installation of the first containers of the 27 new ones that will be installed in the Vidigal favela, in the South Zone of Rio de Janeiro.",
-  communities: ["Rocinha", "Vidigal"],
-  interests: ["Health", "Security"]
+  communities: ["Vidigal", "Rocinha"],
+  interests: ["Health", "Social", "Politics", "Others"]
 )
-article.user = reporters.sample
+article.user = reporters[0]
 article.photo.attach(io: article_photo3, filename: 'article3.jpg', content_type: 'image/jpg')
 article.save!
 articles << article
 
 article = Article.new(
-  title: "Athlete from Rocinha raises a kitty to play in Portugal",
+  title: "Athlete from Chapadão raises a kitty to play in Portugal",
   subtitle: "The crowdfunding seeks to raise 20 thousand reais",
-  content: "What boy has never dreamed of being a professional football player? And when you are a slum child, it intensifies even more, because the chance to live that dream and be able to change your life and that of your family turns out to be an extra motivation. The defender of the Fut 7 amateur team, Almost Nothing F.C., Matheus Nattan dos Santos, 19, resident of Nova Brasília in Complexo do Alemão, is one of the many who are chasing this dream. Having played for the team for a year, Matheus received on June 10th the chance to do assessments for a week in the city of Chaves, in the district of Vila Real, in northern Portugal, and be able to pursue a professional career in Portuguese football. However, travel costs are high, and the athlete decided to create an online kitty to be able to travel. You can contribute by clicking here.",
-  communities: ["Rocinha", "Vidigal"],
-  interests: ["Health", "Security"]
+  content: "What boy has never dreamed of being a professional football player? And when you are a slum child, it intensifies even more, because the chance to live that dream and be able to change your life and that of your family turns out to be an extra motivation. The defender of the Fut 7 amateur team, Almost Nothing F.C., Matheus Nattan dos Santos, 19, resident of Pavuna in Complexo do Chapadão, is one of the many who are chasing this dream. Having played for the team for a year, Matheus received on June 10th the chance to do assessments for a week in the city of Chaves, in the district of Vila Real, in northern Portugal, and be able to pursue a professional career in Portuguese football. However, travel costs are high, and the athlete decided to create an online kitty to be able to travel. You can contribute by clicking here.",
+  communities: ["Chapadão"],
+  interests: ["Sports", "Culture", "Social"]
 )
-article.user = reporters.sample
+article.user = reporters[2]
 article.photo.attach(io: article_photo4, filename: 'article4.jpg', content_type: 'image/jpg')
 article.save!
 articles << article
 
 article = Article.new(
   title: "23-year-old young man creates social project 'Favela Vertical'.",
-  subtitle: "Rafael Capivara brings culture, education and assistance to communities in Tijuca",
+  subtitle: "Rafael Capivara brings culture, education and assistance to communities in Antares",
   content: "Rafael says that the collective emerged in August 2019, along with the construction of his training as a producer by ESPM and the Rio Art Museum. “I always understood that my favela needed a plural space, multiple of ideas and that could rethink like ours. territory can develop. Understanding from the beginning how and why we were stuck in community development compared to other communities because of the political force imposed on us since the emergence of community, I tried to produce an organism that could go beyond and live together with it. And it was with this in mind that I thought of a space for courses, workshops and construction that would provide community education, territorial culture and income generation.”",
-  communities: ["Borel", "Formiga"],
-  interests: ["Sports", "Economy"]
+  communities: ["Antares"],
+  interests: ["Culture", "Economy", "Social", "Politics"]
 )
-article.user = reporters.sample
+article.user = reporters[2]
 article.photo.attach(io: article_photo5, filename: 'article5.jpg', content_type: 'image/jpg')
 article.save!
 articles << article
@@ -221,9 +221,9 @@ article = Article.new(
   title: "Check the Covid-19 vaccination schedule for the week of June 14-19",
   subtitle: "General public, from 53 to 50 years old, will be vaccinated, and Higher Education professionals",
   content: "With the arrival of more vaccines in the city of Rio de Janeiro, the vaccination schedule is suffering advances. Last Wednesday (09) the Municipal Health Department announced once again the acceleration of the immunization schedule against Covid-19. From next Monday (14) until Friday (19), people aged 53 to 50 will be vaccinated, in a staggered way, by age.
-  It is important to note that on Wednesday, June 16, the day is dedicated to workers in Higher Education Education, both from public and private institutions in the city of Rio. be contemplated.",
-  communities: ["Juramento", "Macacos"],
-  interests: ["Sports", "Economy"]
+  It is important to note that on Wednesday, June 23, the day is dedicated to workers in Higher Education Education, both from public and private institutions in the city of Rio. be contemplated.",
+  communities: ["Rocinha", "Vidigal", "Borel", "Formiga", "Macacos", "Alemão", "Antares", "Jacaré", "Chapadão", "Terreirão"],
+  interests: ["Health", "Social", "Others"]
 )
 article.user = reporters.sample
 article.photo.attach(io: article_photo6, filename: 'article6.jpg', content_type: 'image/jpg')
@@ -234,8 +234,8 @@ article = Article.new(
   title: "Cufa launches ‘Ajudar Tá No Sangue’",
   subtitle: "Mobilization wants to reach 5,000 slums across the country",
   content: "This Monday, the Central Única das Favelas (Cufa) launches a national blood donation campaign. The idea is, according to the organizers, to show society the solidarity potential of the favela.",
-  communities: ["Juramento", "Macacos", "Rocinha", "Vidigal", "Borel", "Formiga"],
-  interests: ["Sports", "Economy", "Culture", "Health", "Social", "Security"]
+  communities: ["Rocinha", "Vidigal", "Borel", "Formiga", "Macacos", "Alemão", "Antares", "Jacaré", "Chapadão", "Terreirão"],
+  interests: ["Health", "Social", "Others"]
 )
 article.user = reporters.sample
 article.photo.attach(io: article_photo7, filename: 'article7.jpg', content_type: 'image/jpg')
@@ -246,8 +246,8 @@ article = Article.new(
   title: "‘Dom’, on Amazon Prime, playboy turned outlaw",
   subtitle: "Real case of a young middle-class carioca is recounted on streaming",
   content: "Well-dressed, blue eyes, luxury car and with a stunning blonde in tow, Pedro Dom is a young man above suspicion in the eyes of security guards and doormen in the wealthy South Zone of Rio. front door in luxury mansions and apartments, where his “tram”, hidden in the vehicle, cleans up with jewelry, dollars and even a dog collar studded with diamonds.",
-  communities: ["Juramento", "Macacos", "Rocinha", "Vidigal", "Borel", "Formiga"],
-  interests: ["Sports", "Economy", "Culture", "Health", "Social", "Security"]
+  communities: ["Rocinha", "Vidigal", "Borel", "Formiga", "Macacos", "Alemão", "Antares", "Jacaré", "Chapadão", "Terreirão"],
+  interests: ["Culture", "Security", "Others"]
 )
 article.user = reporters.sample
 article.photo.attach(io: article_photo8, filename: 'article8.jpg', content_type: 'image/jpg')
@@ -258,8 +258,8 @@ article = Article.new(
   title: "Iza exalts the periphery in ‘Gheto’, a clip recorded in the Igreja da Penha",
   subtitle: "The singer's mother goes up on her knees to the Basilica in an emotional scene",
   content: "Born in Pottery, North Zone of Rio, singer and songwriter Iza is back! Released today (04) with an impeccable videography, with the backdrop of Igreja da Penha, the clip ‘Gueto’ is part of his next album that highlights the Rio suburbs and is scheduled to premiere in the 2nd semester. Iza's identity marks the hit, which features scenes recorded in the neighborhood where the singer grew up. In addition to the striking choreography, vibrant colors and the participation of percussionists from the Imperatriz Leopoldinense samba school, the scene in which Isabel Cristina Lima, the singer's mother, climbs the steps of the Sanctuary Basilica of Nossa Senhora da Penha de França, is one of the highlights of the film . The composition of “Gueto” is by IZA, Pablo Bispo, Ruxell and Sérgio Santos, who also produced the clip, which was recorded, mixed and mastered at Pancadão Estúdio.",
-  communities: ["Juramento", "Macacos", "Rocinha", "Vidigal", "Borel", "Formiga"],
-  interests: ["Sports", "Economy", "Culture", "Health", "Social", "Security"]
+  communities: ["Rocinha", "Vidigal", "Borel", "Formiga", "Macacos", "Alemão", "Antares", "Jacaré", "Chapadão", "Terreirão"],
+  interests: ["Culture", "Others"]
 )
 article.user = reporters.sample
 article.photo.attach(io: article_photo9, filename: 'article9.jpg', content_type: 'image/jpg')
@@ -267,13 +267,13 @@ article.save!
 articles << article
 
 article = Article.new(
-  title: "Abandoned for 10 years, residents of Penha fight for the revitalization of park",
+  title: "Abandoned for 10 years, Antares fight for the revitalization of park",
   subtitle: "In search of a solution, local activists went to court",
-  content: "An environment that offered fun and leisure is currently abandoned. The Ary Barroso Park, in Penha, North Zone of Rio, has suffered from neglect for over 10 years. Residents and local leaders joined together and demanded the revitalization of the park. In search of a solution, activists went to court.",
-  communities: ["Juramento", "Macacos", "Rocinha", "Vidigal", "Borel", "Formiga"],
-  interests: ["Sports", "Economy", "Culture", "Health", "Social", "Security"]
+  content: "An environment that offered fun and leisure is currently abandoned. The Ary Barroso Park, in Antares, North Zone of Rio, has suffered from neglect for over 10 years. Residents and local leaders joined together and demanded the revitalization of the park. In search of a solution, activists went to court.",
+  communities: ["Antares"],
+  interests: ["Social", "Security", "Health", "Others"]
 )
-article.user = reporters.sample
+article.user = reporters[2]
 article.photo.attach(io: article_photo10, filename: 'article10.jpg', content_type: 'image/jpg')
 article.save!
 articles << article
