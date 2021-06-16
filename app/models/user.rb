bcdb39d8789f03df5da32e66429906839c17d0be
Augusto Souza <augustodesouzacardoso@gmail.com>
2021-06-16 14:00:38 -0300

    all avatars are round and Add validation in Communities and Interests presence: true to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,12 +13,14 @@ class User < ApplicationRecord
   COMMUNITIES = %w(Rocinha Vidigal Borel Formiga Macacos Alemão Antares Jacaré Chapadão Terreirão)
   INTERESTS = %w(Sports Culture Health Social Security Economy Politics Others)
 
-  validates :communities, inclusion: { in: COMMUNITIES }  
-  validates :interests, inclusion: { in: INTERESTS }  
+  validates :communities, inclusion: { in: COMMUNITIES }
+  validates :interests, inclusion: { in: INTERESTS }
 
   validates :email, presence: true
   validates :first_name, presence: true
   validates :last_name, presence: true
+  validates :communities, presence: true
+  validates :interests, presence: true
   validates :email, format: { with: /\b[\w\.-]+@[\w\.-]+\.\w{2,4}\b/, message: "Invalid email" }
 
   def fullname
